5bb207a481022affac104b044675336a443301d8
Alexandru Enache <alexandru.enache@tremend.com>
2020-02-21 16:10:18 +0200

    e3 - first component

---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {  }
+    render() { 
+        return <p>first component!</p>;
+    }
+}
+ 
+export default Counter;
